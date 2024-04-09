f96bd2207296d46bd42865504ac665e7f06cb41f
fdevia <Francisco_Devia@hotmail.com>
2024-04-09 18:17:36 -0500

    004 commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
+"use client";
 import Image from "next/image";
+import { Box } from "@mui/material";
+import { useWindowSize } from "react-use";
+
 import styles from "./page.module.css";
 
 export default function Home() {
+  const { width, height } = useWindowSize();
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          www.logisticsone.pe under construction Get started by appstogo S.A.S.
-        </p>
-      </div>
+      <>
+        <Box>
+          <Image
+            alt="wwwlogisticsone"
+            src="/images/wwwlogisticsone.png"
+            style={{ width: "100%", height: "100%" }}
+            width={width}
+            height={height}
+            priority
+          />
+        </Box>
+      </>
     </main>
   );
 }
