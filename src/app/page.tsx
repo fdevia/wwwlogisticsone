3480f20e537303228eb500a1a8e147fcb32880e1
fdevia <Francisco_Devia@hotmail.com>
2024-04-09 18:23:36 -0500

    commit 005

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,20 @@ import Image from "next/image";
 import { Box } from "@mui/material";
 import { useWindowSize } from "react-use";
 
-import styles from "./page.module.css";
-
 export default function Home() {
   const { width, height } = useWindowSize();
   return (
-    <main className={styles.main}>
-      <>
-        <Box>
-          <Image
-            alt="wwwlogisticsone"
-            src="/images/wwwlogisticsone.png"
-            style={{ width: "100%", height: "100%" }}
-            width={width}
-            height={height}
-            priority
-          />
-        </Box>
-      </>
-    </main>
+    <>
+      <Box>
+        <Image
+          alt="wwwlogisticsone"
+          src="/images/wwwlogisticsone.png"
+          style={{ width: "100%", height: "100%" }}
+          width={width}
+          height={height}
+          priority
+        />
+      </Box>
+    </>
   );
 }
