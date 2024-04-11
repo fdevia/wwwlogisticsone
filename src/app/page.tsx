81beca6091ce4fb9c273eb021be4ee333d90393e
fdevia <Francisco_Devia@hotmail.com>
2024-04-10 21:01:00 -0500

    commit imageloaded 001

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,62 @@
 "use client";
 import Image from "next/image";
 import { Box } from "@mui/material";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useWindowSize } from "react-use";
 
 export default function Home() {
   const { width, height } = useWindowSize();
-  const [imageLoaded, setImageLoaded] = useState(false);
-  const [currentWidth, setCurrentWidth] = useState(0);
-  const [currentHeight, setCurrentHeight] = useState(0);
-  const [imageWidthDesktop, setImageWidthDesktop] = useState(0);
-  const [imageHeightDesktop, setImageHeightDesktop] = useState(0);
+  const [desktopImageLoaded, setDesktopImageLoaded] = useState(false);
+  const [currentWindowWidth, setCurrentWindowWidth] = useState(0);
+  const [currentWindowHeight, setCurrentWindowHeight] = useState(0);
+  const [currentDesktopImageWidth, setCurrentDesktopImageWidth] = useState(0);
+  const [currentDesktopImageHeight, setCurrentDesktopImageHeight] = useState(0);
 
   useEffect(() => {
-    setCurrentWidth(width);
-    setCurrentHeight(height);
+    setCurrentWindowWidth(Math.min(window.innerWidth, width));
+    setCurrentWindowHeight(Math.min(window.innerHeight, height));
   }, [width, height]);
 
   useEffect(() => {
-    const handleImageLoad = () => {
-      setImageLoaded(true);
-      setImageWidthDesktop(img.width);
-      setImageHeightDesktop(img.height);
-    };
     const img = document.createElement("img");
     img.src = "/images/wwwlogisticsone.png";
-    img.onload = handleImageLoad;
+    img.onload = () => {
+      setDesktopImageLoaded(true);
+      setCurrentDesktopImageWidth(img.width);
+      setCurrentDesktopImageHeight(img.height);
+    };
     img.onerror = () => {
       console.error("Failed to load image:", "/images/wwwlogisticsone.png");
-      setImageLoaded(true);
+      setDesktopImageLoaded(false);
     };
   }, []);
 
   return (
     <>
-      {imageLoaded && currentWidth > 768 && (
-        <Box>
+      <label>{"Window Width "}</label>
+      <label>{currentWindowWidth}</label>
+      <label>{" Window Height "}</label>
+      <label>{currentWindowHeight}</label>
+      <label>{" Image Width "}</label>
+      <label>{currentDesktopImageWidth}</label>
+      <label>{" Image Height "}</label>
+      <label>{currentDesktopImageHeight}</label>
+      {desktopImageLoaded && currentWindowWidth > 768 && (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            overflow: "hidden",
+          }}
+        >
           <Image
             alt="wwwlogisticsone"
             src="/images/wwwlogisticsone.png"
-            style={{ width: "100%", height: "100%" }}
-            width={width}
-            height={height}
+            objectFit="cover"
+            width={currentWindowWidth}
+            height={currentWindowHeight}
             priority
           />
         </Box>
